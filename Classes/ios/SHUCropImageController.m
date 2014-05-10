c3a44be9bb9e26cdc599bb414d36cfd0850eea07
Sergey Shulga <sergey.shulga@softheme.com>
2014-05-10 21:58:50 +0300

    fixed zooming

--- Classes/ios/SHUCropImageController.m
+++ Classes/ios/SHUCropImageController.m
@@ -9,16 +9,16 @@
 #import "SHUCropImageController.h"
 #import "SHUCropView.h"
 
-@interface SHUCropImageController ()
+@interface SHUCropImageController () <UIScrollViewDelegate>
 
-@property (weak, nonatomic) IBOutlet UIScrollView       *scrollView;
-@property (weak, nonatomic) IBOutlet UIImageView        *imageView;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *contectViewWidthConstraint;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *contentViewHeightConstraint;
-@property (weak, nonatomic) IBOutlet UIView             *contentView;
+@property (weak, nonatomic)   IBOutlet UIScrollView       *scrollView;
+@property (weak, nonatomic)   IBOutlet UIImageView        *imageView;
+@property (weak, nonatomic)   IBOutlet NSLayoutConstraint *contectViewWidthConstraint;
+@property (weak, nonatomic)   IBOutlet NSLayoutConstraint *contentViewHeightConstraint;
+@property (weak, nonatomic)   IBOutlet UIView             *contentView;
 
-@property (strong, nonatomic)        UIImage            *imageToCrop;
-@property (assign, nonatomic)        CGSize              cropSize;
+@property (strong, nonatomic)          UIImage            *imageToCrop;
+@property (assign, nonatomic)          CGSize              cropSize;
 
 @end
 
@@ -41,12 +41,15 @@
 - (void) viewDidLoad{
     [super viewDidLoad];
     [self _configView];
+    [self _configScrollViewZoom];
 }
 
-- (void) viewDidLayoutSubviews{
-    [super viewDidLayoutSubviews];
+- (void) viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    
     [self _configScrollViewInsets];
 }
+
 
 #pragma mark - Private
 
@@ -59,6 +62,15 @@
     [self.view addSubview:cropView];
 }
 
+- (void) _configScrollViewZoom{
+    if (self.cropSize.width > self.cropSize.height) {
+        self.scrollView.minimumZoomScale =  self.cropSize.width / self.contectViewWidthConstraint.constant;
+    }else{
+        self.scrollView.minimumZoomScale =  self.cropSize.height / self.contectViewWidthConstraint.constant;
+    }
+    self.scrollView.maximumZoomScale = 20.f;
+}
+
 - (void) _configScrollViewInsets {
     
     CGFloat cropViewHeight = self.cropSize.height;
@@ -67,7 +79,7 @@
     
     CGFloat topInset, bottomInset, leftInset, rightInset;
 
-    topInset = (self.contentView.frame.size.height - cropViewHeight) / 2.f + (navigationBarHeight + statusBarHeight) / 2.f;
+    topInset = (self.contentView.frame.size.height - cropViewHeight) / 2.f - (navigationBarHeight + statusBarHeight) / 2.f;
     bottomInset = (self.contentView.frame.size.height - cropViewHeight) / 2.f - (navigationBarHeight + statusBarHeight) / 2.f;
     leftInset = (self.scrollView.frame.size.width - self.cropSize.width) / 2.f;
     rightInset = leftInset;
@@ -75,26 +87,32 @@
     self.scrollView.contentInset = UIEdgeInsetsMake(topInset, leftInset, bottomInset, rightInset);
 }
 
-- (CGSize ) _sizeOfImage:(UIImage *)image{
+- (CGSize) _sizeOfImage:(UIImage *)image{
     
     CGSize actualSize = image.size;
     CGFloat width, height;
     
-    if (actualSize.width < self.view.frame.size.width) {
-        width = self.view.frame.size.width;
+    if (actualSize.width > self.cropSize.width) {
+        width = self.cropSize.width;
         height = actualSize.height * width / actualSize.width;
-    
+        
     }else{
         height = actualSize.height;
         width = actualSize.width;
     }
     
-    if (height < self.view.frame.size.height) {
-        width = width * self.view.frame.size.height / height;
-        height = self.view.frame.size.height;
+    if (height < self.cropSize.height) {
+        width = width * self.cropSize.height / height;
+        height = self.cropSize.height;
     }
-    
     return CGSizeMake(ceilf(width), ceilf(height));
+}
+
+
+#pragma mark - UIScrollViewDelegate
+
+- (UIView *) viewForZoomingInScrollView:(UIScrollView *)scrollView{
+    return self.contentView;
 }
 
 
