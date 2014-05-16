b17fa64a27a1500683fa925224c17622ec20b320
Sergey Shulga <sergey.shulga@softheme.com>
2014-05-16 22:36:30 +0300

    refactored crop view

--- Classes/ios/SHUCropView.m
+++ Classes/ios/SHUCropView.m
@@ -27,7 +27,7 @@ typedef NS_ENUM(NSInteger, SHUCropViewEdgeType) {
     self = [super initWithFrame:frame];
     if (self) {
         self.cropSize = cropSize;
-        self.backgroundColor = [UIColor clearColor];
+        self.backgroundColor = [UIColor colorWithWhite:0.f alpha:0.5];
         self.userInteractionEnabled = NO;
     }
     return self;
@@ -39,7 +39,6 @@ typedef NS_ENUM(NSInteger, SHUCropViewEdgeType) {
     CGContextRef context = UIGraphicsGetCurrentContext();
     
     [self _drawCenterRectInContext:context];
-    [self _drawOtherRectsInContext:context];
 }
 
 #pragma mark - Private
@@ -49,33 +48,10 @@ typedef NS_ENUM(NSInteger, SHUCropViewEdgeType) {
     CGContextSetStrokeColorWithColor(context,
                                      [UIColor whiteColor].CGColor);
     CGRect rectangle = [self _centerRect];
+    
+    CGContextClearRect(context, rectangle);
     CGContextAddRect(context, rectangle);
     CGContextStrokePath(context);
-}
-
-- (void) _drawOtherRectsInContext:(CGContextRef )context{
-    
-    CGContextSetFillColorWithColor(context, [UIColor colorWithWhite:0.f alpha:0.5f].CGColor);
-    
-    CGRect rectangleLeft = [self _rectForEdgeType:SHUCropViewEdgeTypeLeft];
-    
-    CGRect rectangleBottom = [self _rectForEdgeType:SHUCropViewEdgeTypeBottom];
-    
-    CGRect rectangleRight = [self _rectForEdgeType:SHUCropViewEdgeTypeRight];
-    
-    CGRect rectangleTop = [self _rectForEdgeType:SHUCropViewEdgeTypeTop];
-    
-    CGContextAddRect(context, rectangleLeft);
-    CGContextFillRect(context, rectangleLeft);
-    
-    CGContextAddRect(context, rectangleBottom);
-    CGContextFillRect(context, rectangleBottom);
-    
-    CGContextAddRect(context, rectangleRight);
-    CGContextFillRect(context, rectangleRight);
-    
-    CGContextAddRect(context, rectangleTop);
-    CGContextFillRect(context, rectangleTop);
 }
 
 - (CGRect ) _centerRect{
@@ -87,43 +63,6 @@ typedef NS_ENUM(NSInteger, SHUCropViewEdgeType) {
     size = self.cropSize;
     
     return CGRectMake(origin.x , origin.y, size.width - 1.f, size.height - 1.f);
-}
-
-- (CGRect ) _rectForEdgeType:(SHUCropViewEdgeType )edgeType{
-    
-    CGPoint origin;
-    CGSize  size;
-    CGRect centerRect = [self _centerRect];
-    
-    switch (edgeType) {
-        case SHUCropViewEdgeTypeLeft:
-            origin = self.bounds.origin;
-            size = CGSizeMake(origin.x + centerRect.origin.x,
-                              self.frame.size.height);
-            break;
-        case SHUCropViewEdgeTypeBottom:
-            origin = CGPointMake(centerRect.origin.x,
-                                 centerRect.origin.y + centerRect.size.height + 1.f);
-            size = CGSizeMake(centerRect.size.width,
-                              self.frame.size.height - origin.y);
-            break;
-        case SHUCropViewEdgeTypeRight:
-            origin = CGPointMake(centerRect.origin.x + centerRect.size.width,
-                                 self.bounds.origin.y);
-            size = CGSizeMake(self.frame.size.width - origin.x,
-                              self.frame.size.height);
-            break;
-        case SHUCropViewEdgeTypeTop:
-            origin = CGPointMake(centerRect.origin.x,
-                                 self.bounds.origin.y);
-            size = CGSizeMake(centerRect.size.width,
-                              centerRect.origin.y);
-            break;
-        default:
-            break;
-    }
-    
-    return CGRectMake(origin.x , origin.y, size.width, size.height);
 }
 
 @end
